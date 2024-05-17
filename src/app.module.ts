a2fa368ac4dd095fdaf648fe57c04c972a994dba
dcamachom <you@example.com>
2024-05-16 20:08:03 -0500

    inic

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial',
+      entities: [],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+ })
+ export class AppModule {}
